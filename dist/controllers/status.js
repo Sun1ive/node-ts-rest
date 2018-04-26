f599966fb31bc9dd0d59ad0b5f129087eb2ef2cf
Sun1ive <sunliveua@gmail.com>
2018-04-26 11:48:21 +0300

    123

--- dist/controllers/status.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hi = (req, res) => {
-    res.send("Hello");
-};
-exports.awesome = (req, res) => {
-    res.send("Awesome");
-};
-//# sourceMappingURL=status.js.map
