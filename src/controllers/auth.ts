e016345fa7daada9789232efa3a7e7517de35ad7
Sun1ive <sunliveua@gmail.com>
2018-06-23 12:28:27 +0300

    upd

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,14 +1,23 @@
+import * as jwt from 'jsonwebtoken';
+import * as uuid from 'uuid';
 import { compareSync } from 'bcryptjs';
-import { findUser } from '../../tests/___mocks___/user';
 import { IRouterContext } from 'koa-router';
+
+import { findUser } from '../../tests/___mocks___/user';
+import config from '../config';
 
 export const login = async (ctx: IRouterContext) => {
   const { login, password }: { login: string; password: string } = ctx.request.body;
   const user = await findUser(login);
   if (!user || !compareSync(password, user.password)) {
     ctx.throw(403);
-  }
+  } else {
+    const refreshToken = uuid();
 
-  ctx.status = 200;
-  ctx.body = user;
+    ctx.status = 200;
+    ctx.body = {
+      token: jwt.sign({ id: user.id }, config.jwtKey),
+      refreshToken,
+    };
+  }
 };
